3ad0a5d973c00ea4c8550923dbe622a181a3cd17
Akbar30Bill <alishahverdi2010@yahoo.com>
2018-09-20 23:56:47 +0430

    deep working?

--- pyGrabber.py
+++ pyGrabber.py
@@ -68,6 +68,10 @@ def find_links_of( url , search_level):
         print(Fore.RED + "Passing the link because of URL is unreachable")
         print(Style.RESET_ALL , end="")
         pass
+    except KeyboardInterrupt:
+        if input('\nAre you sure you want to quit (y/n)? : ') == 'y':
+            sys.exit(0)
+        print("Continue:")
     except:
         print(Fore.RED + "The read operation timed out")
         print(Style.RESET_ALL , end="")
@@ -78,20 +82,27 @@ append_file_content = []
 
 for x in range(0 , search_level):
     for y in range(0 , len(file_content)):
-        append_file_content.append(find_links_of(file_content[y] , x))
-    file_content = file_content + append_file_content
+        new_links = file_content(file_content[y] , x)
+        if new_links not in file_content and new_links not in append_file_content:
+            append_file_content = append_file_content + new_links
+        # append_file_content.append(find_links_of(file_content[y] , x))
+    file_content = append_file_content
     append_file_content = []
 
 
 for x in file_content:
-    if x.endswith(file_format):
-        files_to_download.append(x)
+    try:
+        if x.endswith(file_format):
+            files_to_download.append(x)
+    except AttributeError:
+        print(x)
+        pass
 
 downloading(file_content , files_to_download)
 
-for x in files_to_download:
-    print("downloading : " , end = '')
-    print(x)
-    print('\t as : ' , end = '')
-    print(x[x.rfind("/")+1:] + '\n')
+# for x in files_to_download:
+    # print("downloading : " , end = '')
+    # print(x)
+    # print('\t as : ' , end = '')
+    # print(x[x.rfind("/")+1:] + '\n')
     # urllib.request.urlretrieve(x , save_directory + '/' + x[x.rfind("/")+1:] )
