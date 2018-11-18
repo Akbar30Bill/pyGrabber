bb44297161a16b7e53f3a5faeb5e77ed2e5bf389
akbar30bill <alishahverdi2010@yahoo.com>
2018-11-18 09:06:54 +0330

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+setup(
+	name = "pyGrabber",
+	version = "0.0.2",
+	author = "Akbar30Bill , MameD",
+	author_email = "alishahverdi2010@yahoo.com",
+	description = "a simple websucker in python",
+	url = "https://github.com/Akbar30Bill/pyGrabber.git",
+	packages = ["pyGrabber" , "update_pyGrabber"],
+	long_description=read('README'),
+	)
